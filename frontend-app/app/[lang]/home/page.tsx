0a1db647fbcdcd8fb334dbac8e6a6dad86bdc56b
Robert <robertosalazarvasquezalexis@gmail.com>
2026-02-09 14:22:25 -0800

    minor fix

--- frontend-app/app/[lang]/home/page.tsx
+++ frontend-app/app/[lang]/home/page.tsx
@@ -28,7 +28,7 @@ export default function Home({ params }: { params: { lang: string } }) {
 				</Link>
 
 				<Link
-					href={`/${lang}/notes`}
+					href={`/notes`}
 					className="px-4 py-2 bg-gray-100 text-gray-800 rounded hover:bg-gray-200"
 				>
 					Notes
