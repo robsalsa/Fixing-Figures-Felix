6514facf511cd6b23a7146a34460956c94b63004
Robert <robertosalazarvasquezalexis@gmail.com>
2026-03-02 14:15:35 -0800

    rewire

--- frontend-app/app/layout.tsx
+++ frontend-app/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : "https://fixing-figures-felix-pilk.vercel.app/";
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
