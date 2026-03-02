58abae02905b17d4b0c9f8c21f05230f5c49da05
Robert <robertosalazarvasquezalexis@gmail.com>
2026-03-02 14:07:17 -0800

    analytics is now up hopfully

--- frontend-app/app/page.tsx
+++ frontend-app/app/page.tsx
@@ -1,4 +1,6 @@
 import { redirect } from "next/navigation";
+// import {Analytics} from "@vercel/analytics/next"
+
 
 export default function RootPage() {
   // Redirect to English homepage by default
