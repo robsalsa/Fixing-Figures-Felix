75d92b7c413017631b8c0325ef66a8101ca06db6
Robert <robertosalazarvasquezalexis@gmail.com>
2026-03-26 08:58:18 -0700

    hotfix 2

--- frontend-app/app/[lang]/home/page.tsx
+++ frontend-app/app/[lang]/home/page.tsx
@@ -25,6 +25,10 @@ function FooterSkeleton() {
 	);
 }
 
+type HomePageProps = {
+	params: Promise<{ lang: string }>;
+};
+
 export default function HomePage({ params }: HomePageProps) {
 	return (
 		<Suspense fallback={<main />}>
